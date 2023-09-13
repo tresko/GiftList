1229b807d7f7c61921dce28519b35035c42fe32f
Miha Sedej <sedej.miha@gmail.com>
2023-09-13 12:13:14 +0200

    Finish week 2

--- client/index.js
+++ client/index.js
@@ -1,17 +1,23 @@
-const axios = require('axios');
-const niceList = require('../utils/niceList.json');
-const MerkleTree = require('../utils/MerkleTree');
+const axios = require('axios')
+const niceList = require('../utils/niceList.json')
+const MerkleTree = require('../utils/MerkleTree')
 
-const serverUrl = 'http://localhost:1225';
+const serverUrl = 'http://localhost:1225'
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  // TODO: how do we prove to the server we're on the nice list?
+  const merkleTree = new MerkleTree(niceList)
 
-  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
-  });
+  const name = 'Miha Sedej'
+  const index = niceList.findIndex(n => n === name)
+  const proof = merkleTree.getProof(index)
 
-  console.log({ gift });
+  const {data: gift} = await axios.post(`${serverUrl}/gift`, {
+    name,
+    proof,
+  })
+
+  console.log({gift})
 }
 
-main();
+main()
--- server/index.js
+++ server/index.js
@@ -1,29 +1,27 @@
-const express = require('express');
-const verifyProof = require('../utils/verifyProof');
+const express = require('express')
+const verifyProof = require('../utils/verifyProof')
 
-const port = 1225;
+const port = 1225
 
-const app = express();
-app.use(express.json());
+const app = express()
+app.use(express.json())
 
 // TODO: hardcode a merkle root here representing the whole nice list
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const MERKLE_ROOT = '31bcc98e6de0935b6558687871023458f6b685d21ad812597958a3ff62f3ef08'
 
 app.post('/gift', (req, res) => {
   // grab the parameters from the front-end here
-  const body = req.body;
+  const {name = '', proof = ''} = req.body
 
-  // TODO: prove that a name is in the list 
-  const isInTheList = false;
-  if(isInTheList) {
-    res.send("You got a toy robot!");
+  const isInTheList = verifyProof(proof, name, MERKLE_ROOT)
+  if (isInTheList) {
+    res.send('You got a toy robot!')
+  } else {
+    res.send('You are not on the list :(')
   }
-  else {
-    res.send("You are not on the list :(");
-  }
-});
+})
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}!`);
-});
+  console.log(`Listening on port ${port}!`)
+})
